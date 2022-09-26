9cd91a28fbfbc0a2a90d031933e9ada2bd2abe52
Kael Renaud <Kmans2272@gmail.com>
2022-09-26 09:54:43 -0500

    hey people

---
+++ Inheritance/Animals.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+
+    internal class Animals
+    {
+        public int legs;
+        public int eyes;
+        public int  age;
+        public string name;
+    }
+    internal class Birds : Animals
+    {
+        public bool canFly;
+        public bool eatsMeat;
+        public string favFood;
+        public bool canSwim;
+    }
+    internal class Reptiles : Animals
+    {
+        public string scalesOrScutes;
+        public bool layEggs;
+        public int lungsNum;
+        public string typeOfReptile;
+    }
+
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+
+
 
 namespace Inheritance
 {
@@ -28,12 +31,45 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Birds b1 = new Birds();
+
+            b1.favFood = "fish";
+            b1.canFly = true;
+            b1.canSwim = true;
+            b1.legs = 2;
+            b1.eyes = 2;
+            b1.age = 3;
+            b1.name = "Bob";
+            b1.eatsMeat = true;
+
+            Console.WriteLine($"{b1.name} the pelican loves {b1.favFood}! True or false... can it fly? {b1.canFly}! True or false... Can it swim? {b1.canSwim}! {b1.name} the pelican has {b1.legs} legs, {b1.eyes} eyes, and he is {b1.age} years old. True or false... Does he eat meat? {b1.eatsMeat}");
+
+
+
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
+
+
+
+            Reptiles r1 = new Reptiles();
+
+            r1.scalesOrScutes = "scales";
+            r1.layEggs = true;
+            r1.lungsNum = 2;
+            r1.legs = 4;
+            r1.eyes = 2;
+            r1.age = 7;
+            r1.name = "Komdrag";
+            r1.typeOfReptile = "Komodo Dragon";
+
+            Console.WriteLine($"{r1.name} the {r1.typeOfReptile} has {r1.scalesOrScutes}! True or false... can it lay eggs? {r1.layEggs}! How many lungs does it have? {r1.lungsNum}! {r1.name} the Komodo Dragon has {r1.legs} legs, {r1.eyes} eyes, and he is {r1.age} years old.");
+
+
+
         }
     }
 }
